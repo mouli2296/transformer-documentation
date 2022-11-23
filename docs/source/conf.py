1a69022da9b78e50a7fe5fbe158c783a80667b6d
mouli <mouli.murakambat@tigeranalytics.com>
2022-11-23 20:12:42 +0530

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Feature Kit'
+copyright = '2021, Tiger Analytics'
+author = 'Tiger Analytics'
 
 release = '0.1'
 version = '0.1.0'
